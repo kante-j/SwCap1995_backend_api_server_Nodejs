446ed944df43ce8ef4b1298521659b5652cfd9c6
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-04-29 20:42:39 +0900

    add user

--- migrations/20200418053520-create-user.js
+++ migrations/20200418053520-create-user.js
@@ -1,27 +1,53 @@
 'use strict';
 module.exports = {
-up: (queryInterface, Sequelize) => {
-  return queryInterface.createTable('Users', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            email: {
+              allowNull: false,
+                type: Sequelize.STRING(30),
+            },
+            password: {
+              allowNull: false,
+                type: Sequelize.STRING(30),
+            },
+            sex: {
+              allowNull: false,
+                type: Sequelize.STRING(10),
+            },
+            age: {
+              allowNull: false,
+              defaultValue:0,
+                type: Sequelize.INTEGER
+            },
+            is_face_detection: {
+              allowNull: true,
+                type: Sequelize.BOOLEAN
+            },
+            interest_category: {
+              allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+            weight: {
+              allowNull: true,
+                type: Sequelize.INTEGER,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+        });
     },
-    userId: {
-      type: Sequelize.STRING
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-},
     down: (queryInterface, Sequelize) => {
-  return queryInterface.dropTable('Users');
-}
+        return queryInterface.dropTable('Users');
+    }
 };
--- models/user.js
+++ models/user.js
@@ -1,10 +1,52 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    userId: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+    const User = sequelize.define('User', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+        },
+        email: {
+            allowNull: false,
+            type: DataTypes.STRING(30),
+        },
+        password: {
+            allowNull: false,
+            type: DataTypes.STRING(30),
+        },
+        sex: {
+            allowNull: false,
+            type: DataTypes.STRING(10),
+        },
+        age: {
+            allowNull: false,
+            defaultValue: 0,
+            type: DataTypes.INTEGER
+        },
+        is_face_detection: {
+            allowNull: true,
+            type: DataTypes.BOOLEAN
+        },
+        interest_category: {
+            allowNull: true,
+            type: DataTypes.INTEGER,
+        },
+        weight: {
+            allowNull: true,
+            type: DataTypes.INTEGER,
+        },
+        createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+        },
+    }, {});
+    User.associate = function (models) {
+        // associations can be defined here
+    };
+    return User;
 };
