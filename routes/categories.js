43077b56b004f50031a77d9cdaff76dede6d5485
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-05-07 20:39:19 +0900

    add test

--- routes/categories.js
+++ routes/categories.js
@@ -4,8 +4,11 @@ var router = express.Router();
 const {category} = require('../models');
 
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.redirect('graphql?query={categoryGet{id,name}}');
 });
 
+router.get('/search', function (req, res) {
+    res.send(req.query.query);
+});
 module.exports = router;
