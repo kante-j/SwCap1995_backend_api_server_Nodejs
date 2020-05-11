1aec347240374a36aa1119c4c3a3a7b0c78ae526
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-05-11 21:05:48 +0900

    카테고리 테스트 추가

--- routes/categories.js
+++ routes/categories.js
@@ -5,7 +5,11 @@ const {category} = require('../models');
 
 
 router.get('/', function (req, res) {
-    res.redirect('graphql?query={categoryGet{id,name}}');
+    res.redirect('graphql?query={categoryGet{name, description}}');
+});
+
+router.get('/image', function (req, res) {
+    res.send('https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg');
 });
 
 router.get('/search', function (req, res) {
