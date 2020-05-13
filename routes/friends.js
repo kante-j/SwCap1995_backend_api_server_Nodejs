9efd7f1eccf946e5e3faa2fa53c311e1eb90c021
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-05-13 20:54:08 +0900

    대기중인 친구목록

--- routes/friends.js
+++ routes/friends.js
@@ -22,6 +22,30 @@ router.get('/:user_id', function (req, res) {
             attributes: ['user_id', 'friend_id', 'isaccept'],
             where: {
                 user_id: req.params.user_id,
+                isaccept: 'accept'
+            },
+        }],
+
+    }).then((user) => {
+        res.send(user);
+    }).catch(err => {
+        console.log(err);
+        res.send(500);
+    });
+});
+
+
+router.get('/waiting/:user_id', function (req, res) {
+    console.log(new Date());
+
+    user.findAndCountAll({
+        include: [{
+            model: friend,
+            as: "friend",
+            attributes: ['user_id', 'friend_id', 'isaccept'],
+            where: {
+                friend_id: req.params.user_id,
+                isaccept: 'waiting'
             },
         }],
 
@@ -46,13 +70,25 @@ router.put('/add', function (req, res) {
             nickname: response.target_nickname
         }
     }).then((target_user) => {
-        friend.create({
-            user_id: response.user_id,
-            friend_id: target_user.id,
-            isaccept: 'waiting',
-            created_at: Date.now(),
-        })
-        res.send(200)
+        //친구목록에 찾아서 없으면 friends 테이블에 추가
+        friend.findOne({
+            where:{
+                user_id: response.user_id,
+                friend_id: target_user.id
+            }
+        }).then((user) =>{
+            console.log("친구목록에 존재합니다!")
+        }).catch(err =>{
+            console.log("친구목록 생성 완료")
+            friend.create({
+                user_id: response.user_id,
+                friend_id: target_user.id,
+                isaccept: 'waiting',
+                created_at: Date.now(),
+            });
+            res.send(200);
+        });
+
     }).catch((err) => {
         console.log(err);
         res.send(500)
