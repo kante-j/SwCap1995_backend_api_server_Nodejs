be76cf25d007d3039c27f5d684c98ccb8dd023ad
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-05-17 21:17:26 +0900

    add app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoriesRouter = require('./routes/categories');
 var friendsRouter = require('./routes/friends');
+var plansRouter = require('./routes/plans');
 var testRouter = require('./routes/test');
 var {GraphQLSchema} = require('graphql');
 var bodyParser = require('body-parser');
@@ -38,6 +39,7 @@ app.use('/users', usersRouter);
 app.use('/categories', categoriesRouter);
 app.use('/friends', friendsRouter);
 app.use('/test', testRouter);
+app.use('/plans', plansRouter);
 
 app.use('/graphql', graphqlHTTP({
     schema: new GraphQLSchema(generateSchema(models)),
