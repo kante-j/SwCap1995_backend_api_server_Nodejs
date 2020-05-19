541d4d88bdb2c75d4776ab7141ce9c784a286540
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-05-19 11:53:52 +0900

    logger 수정

--- logger.js
+++ logger.js
@@ -17,19 +17,59 @@ var winston = require('winston');
 // Requiring `winston-papertrail` will expose
 // `winston.transports.Papertrail`
 //
-require('winston-papertrail').Papertrail;
+var Papertrail = require('winston-papertrail').Papertrail;
+var logger;
+var consoleLogger = consoleLogger = new winston.transports.Console({
+    level: 'debug',
+    timestamp: function() {
+        return new Date().toString();
+    },
+    colorize: true
+});
+
+
+var ptTransport = new Papertrail({
+    host: 'logs2.papertrailapp.com',
+    port: 14880,
+    hostname: 'planA',
+    levels: ['debug','info','warn','error'],
+    logFormat: function(level, message) {
+        return '[' + level + '] ' + message;
+    }
+});
+
+
+ptTransport.on('error', function(err) {
+    logger && logger.error(err);
+});
+
+ptTransport.on('connect', function(message) {
+    logger && logger.info(message);
+});
 
 var logger = new winston.createLogger({
-    transports: [
-        new winston.transports.Papertrail({
-            host: 'logs2.papertrailapp.com',
-            port: 14880,
-            logFormat: function(level, message) {
-                return '<<<' + level + '>>> ' + message;
-            }
-        })
+    levels:{
+        debug: 0,
+        info: 1,
+        warn: 2,
+        error: 3
+    },
+    transports:[
+        consoleLogger,
+        ptTransport
     ]
+    // transports: [
+    //     new winston.transports.Papertrail({
+    //         host: 'logs2.papertrailapp.com',
+    //         port: 14880,
+    //         level: 'debug',
+    //         logFormat: function(level, message) {
+    //             return '<<<' + level + '>>> ' + message;
+    //         }
+    //     })
+    // ]
 });
+
 
 logger.info('this is my message');
 
