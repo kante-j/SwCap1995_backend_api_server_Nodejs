9de607ef1f198882eb088b7cfeb18240f906cfd5
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-06-08 20:51:02 +0900

    일일인증 이미지 업로드

--- routes/dailyAuthentications.js
+++ routes/dailyAuthentications.js
@@ -2,6 +2,38 @@ var express = require('express');
 var router = express.Router();
 const {user, plan, watcher, point, agreement, daily_authentication} = require('../models');
 
+const multer = require("multer");
+const multerS3 = require('multer-s3');
+const secretKey = require('../secretKey')
+const AWS = require('aws-sdk');
+
+const endpoint = new AWS.Endpoint('https://kr.object.ncloudstorage.com');
+const region = 'kr-standard';
+const access_key = secretKey.ACCESS_KEY;
+const secret_key = secretKey.SECRET_KEY;
+
+const s3 = new AWS.S3({
+    endpoint: endpoint,
+    region: region,
+    credentials: {
+        accessKeyId : access_key,
+        secretAccessKey: secret_key
+    },
+});
+
+const uploadImage = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'swcap1995/dailyAuthentications',
+        acl: 'public-read',
+        metadata(req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key(req, file, cb) {
+            cb(null, Date.now().toString() + '.png');
+        }
+    })
+});
 
 router.get('/:plan_id',function (req, res) {
     console.log(new Date());
@@ -23,13 +55,13 @@ router.get('/:plan_id',function (req, res) {
     });
 });
 
-router.post('/',function (req, res) {
+router.post('/',uploadImage.single('photo'),function (req, res) {
     console.log(new Date());
     let response = {
         user_id: req.body.user_id,
         plan_id: req.body.plan_id,
         status: req.body.status,
-        image_url: req.body.image_url,
+        image_url: 'https://kr.object.ncloudstorage.com/swcap1995/dailyAuthentications/'+req.file.key,
         comment: req.body.comment,
     };
 
@@ -41,7 +73,6 @@ router.post('/',function (req, res) {
         image_url: response.image_url,
         comment: response.comment,
     }).then((daily_auth) =>{
-
         res.sendStatus(200);
     }).catch(err =>{
         console.log(err);
