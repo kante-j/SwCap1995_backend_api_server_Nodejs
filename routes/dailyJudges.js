96a0a638c390ded980c2e56e401b664b3d9473bc
wltjr0920 <wltjr0920@ajou.ac.kr>
2020-06-13 16:48:20 +0900

    일일 감시 여부

--- routes/dailyJudges.js
+++ routes/dailyJudges.js
@@ -2,6 +2,32 @@ var express = require('express');
 var router = express.Router();
 const {user, plan, friend, watcher, daily_judge, daily_authentication} = require('../models');
 
+router.post('/is_exist', function (req, res) {
+    console.log(new Date());
+
+    let response = {
+        user_id: req.body.user_id,
+        daily_auth_id: req.body.daily_auth_id
+    };
+
+    daily_judge
+        .findAndCountAll({
+        where:{
+            user_id:response.user_id,
+            daily_auth_id:response.daily_auth_id,
+        }
+    }).then(daily_judge =>{
+        if(daily_judge.count != 0){
+            res.sendStatus(500)
+        }else{
+            res.sendStatus(200);
+        }
+    }).catch(err=>{
+        console.log(err);
+        res.sendStatus(503);
+    })
+});
+
 router.post('/', function (req, res) {
     console.log(new Date());
     let response = {
